a0c0a94b3d8c6dee31cd1b2f781c56ee31df83b0
Benjamin F Jones <benjaminfjones@gmail.com>
2023-10-19 22:44:24 +0000

    Add range_offset with termination proof

--- Common.lean
+++ Common.lean
@@ -3,8 +3,7 @@
 
 namespace List
   /- TODO Do we actually need `end_itlist` ?-/
-  def end_itlist [Inhabited α] (f: α → α → α) (l: List α) : α :=
-    match l with
+  def end_itlist [Inhabited α] (f: α → α → α) : List α → α
     | [] => sorry
     | [x] => x
     | h :: t => f h (end_itlist f t)
@@ -19,8 +18,7 @@ namespace List
   example : all_pairs (fun x y => x * y) [1,2,3] [-1, -2, -3] =
     [-1, -2, -3, -2, -4, -6, -3, -6, -9] := by rfl
 
-  def distinct_pairs (l : List α) : List (α × α) :=
-    match l with
+  def distinct_pairs : List α → List (α × α)
     | [] => []
     | x :: t => foldr (fun y a => (x, y) :: a) (distinct_pairs t) t
   
@@ -33,8 +31,22 @@ namespace List
     if j < i then [] else i :: range_from (i+1) j
 
   #eval range_from 1 4
-  /- example : range_from 1 4 = [1, 2, 3, 4] := by rfl -/
+  /-
+  example : range_from 1 4 = [1, 2, 3, 4] := by
+    simp [range_from]  -- simp doesn't make progress, perhaps because it doesn't know it terminates?
+  -/
   
+  def range_offset (i : Nat) : Nat → List Nat
+    | 0 => []
+    | Nat.succ k => i :: range_offset (i+1) k
+  termination_by range_offset i k => k
+  decreasing_by
+    simp_wf
+    apply Nat.lt_succ_self
+  
+  #eval range_offset 1 4
+  example : range_offset 1 4 = [1, 2, 3, 4] := by rfl
+
   def non (p : α → Bool) (x: α) : Bool := not (p x)
   example : map (non (fun x => x % 2 = 0)) [0, 1, 2] = [false, true, false] := by rfl
 
